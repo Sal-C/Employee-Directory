d728bbcb177ccb83282c5fe42de423dfc55ef9d2
Sal <salvadorchaveztrejo@gmail.com>
2020-06-04 15:08:39 -0700

    Updates

---
+++ precache-manifest.795a3af29ca25f61ed9278233c27f3ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8d56254087d188e0f316873a26979c",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "3d08c8212087dbdbf0c6",
+    "url": "/Employee-Directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff629fde24fb9965f558",
+    "url": "/Employee-Directory/static/js/2.f91cda88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.f91cda88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d08c8212087dbdbf0c6",
+    "url": "/Employee-Directory/static/js/main.30b2271c.chunk.js"
+  },
+  {
+    "revision": "86ddd39ac8bda3229f19",
+    "url": "/Employee-Directory/static/js/runtime-main.b9ccabc3.js"
+  }
+]);
